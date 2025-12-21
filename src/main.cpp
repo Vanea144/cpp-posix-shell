6fce14e0f99f00b90370d8b0f941df1003af07bd
Ion Zlotea <i.zlotea@student.tudelft.nl>
2025-12-21 13:04:26 +0100

    REPL

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  	// Flush after every std::cout / std:cerr
+  	std::cout << std::unitbuf;
+  	std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-  std::string s;
-  std::getline(std::cin, s);
-  std::cout << s + ": command not found\n"; 
+	std::string s;
+	std::cout << "$ ";
+
+	while(std::getline(std::cin, s)) {
+		std::cout << s + ": command not found\n"; 	
+		std::cout << "$ ";
+	}
 }
