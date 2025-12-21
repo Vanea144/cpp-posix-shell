1dd5c14c7757d4967e7227db7f2d8e84920abf5b
Ion Zlotea <i.zlotea@student.tudelft.nl>
2025-12-21 13:33:50 +0100

    type

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,26 @@ int main() {
 
 	std::string s;
 
+	std::vector<std::string> commands = {"exit", "type", "echo"};
+
 	while(std::cout << "$ ", std::getline(std::cin, s)) {
 		if(s.substr(0, 4) == "exit") return 0;
+		std::string message = s.substr(5);
 		if(s.substr(0, 4) == "echo") {
-			std::string message = s.substr(5);
 			std::cout << message << '\n';
+			continue;
+		}
+		if(s.substr(0, 4) == "type") {
+			std::cout << message;
+			for(std::string cmnd : commands) {
+				if(cmnd == message) {
+					std::cout << " is a shell builtin\n";
+					break;
+				}
+				if(cmnd == commands.back()) {
+					std::cout << ": not found\n";
+				}
+			}
 			continue;
 		}
 		std::cout << s + ": command not found\n"; 	
