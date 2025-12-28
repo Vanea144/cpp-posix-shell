ac2909b5834083f6c5d0da5859c22e66885c282e
Ion Zlotea <i.zlotea@student.tudelft.nl>
2025-12-29 00:01:17 +0200

    Add support for \ within double quotes

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ std::vector<std::string> tokenize(const std::string& input) {
 	std::vector<std::string> tokens;
 	std::string current_token = "";
 	bool in_squotes = false, in_dquotes = false;
-	std::vector<char> escaped = {'"', '`', '$', '\\'};
+	std::vector<char> escaped = {'"', '\\'};
 
 	for(int i = 0; i < (int)input.size(); i++) {
 		char c = input[i];
@@ -46,17 +46,14 @@ std::vector<std::string> tokenize(const std::string& input) {
 		else if(c == '\\' && in_dquotes) {
 			++i;
 			char next_c = input[i];
-			if(next_c == 'n') current_token += '\n';
-			else {
-				for(auto it : escaped) {
-					if(next_c == it) {
-						current_token += it;
-						break;
-					}
-					if(it == escaped.back()) {
-						--i;
-						current_token += '\\';
-					}
+			for(auto it : escaped) {
+				if(next_c == it) {
+					current_token += it;
+					break;
+				}
+				if(it == escaped.back()) {
+					--i;
+					current_token += '\\';
 				}
 			}
 		}
